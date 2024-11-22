b9268bfe5cc3eddeaea13d2e8e600c396a22f5f7
Jacob Heric <jacob@jacobheric.com>
2024-11-22 15:54:51 -0500

    try catch around our crazy stream parser

--- islands/search.tsx
+++ islands/search.tsx
@@ -1,7 +1,6 @@
-import { SONGS } from "@/lib/signals/songs.ts";
-import { useState } from "preact/hooks";
-import { ERROR } from "@/lib/signals/songs.ts";
+import { ERROR, SONGS } from "@/lib/signals/songs.ts";
 import { type FormEvent } from "preact/compat";
+import { useState } from "preact/hooks";
 
 const NOT_FOUND = "No songs found, try adjusting your prompt.";
 
@@ -38,18 +37,23 @@ export const Search = ({ test }: { test?: boolean }) => {
       return;
     }
 
-    const reader = response.body.pipeThrough(new TextDecoderStream())
-      .getReader();
+    try {
+      const reader = response.body.pipeThrough(new TextDecoderStream())
+        .getReader();
 
-    while (true) {
-      const { value, done } = await reader.read();
-      if (done) {
-        break;
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          break;
+        }
+        const parsed = parseSong(value);
+        if (parsed) {
+          SONGS.value = [...SONGS.value, ...[parsed]];
+        }
       }
-      const parsed = parseSong(value);
-      if (parsed) {
-        SONGS.value = [...SONGS.value, ...[parsed]];
-      }
+    } catch (e) {
+      console.error("error reading songs", e);
+      ERROR.value = "There was an error searching songs. Please try again.";
     }
 
     setSubmitting(false);
@@ -73,24 +77,27 @@ export const Search = ({ test }: { test?: boolean }) => {
             className="rounded-b-none"
           />
           {
-            /* <div className="flex flex-row justify-start items-center gap-1 border border-gray-200 dark:bg-gray-900 rounded px-3 border-t-0 p-2 rounded-t-none">
-          <div className="w-16">I prefer:</div>
-          <select
-            name="mode"
-            id="mode"
-            className="w-[65px] p-0 text-right"
-          >
-            <option selected={mode === "smart"} className="" value="smart">
-              smart
-            </option>
-            <option selected={mode === "fast"} className="" value="fast">
-              fast
-            </option>
-            <option selected={mode === "recent"} className="" value="recent">
-              recent
-            </option>
-          </select>
-        </div> */
+            /* <div className="flex flex-row justify-between items-center gap-1 border border-gray-200 dark:bg-gray-900 rounded px-3 border-t-0 p-2 rounded-t-none">
+            <select
+              name="mode"
+              id="mode"
+              className="h-6 w-auto min-w-fit pr-6 py-0 px-2 text-xs text-gray-700"
+            >
+              <option value="smart">
+                prefer smart
+              </option>
+              <option value="recent">
+                prefer recent
+              </option>
+            </select>
+            {submitting && (
+              <div className="flex flex-row justify-start items-center animate-pulse">
+                <Logo className="w-4 fill-gray-900 dark:fill-white animate-spin" />
+                {" "}
+                utherizing...
+              </div>
+            )}
+          </div> */
           }
         </div>
         <button
