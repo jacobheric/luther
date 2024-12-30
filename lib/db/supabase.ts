c2f980368e1d98b85b9c8107c79c41a2a386c0e9
Jacob Heric <jacob@jacobheric.com>
2024-12-30 16:32:15 -0500

    assert supabase env vars

--- lib/db/supabase.ts
+++ lib/db/supabase.ts
@@ -1,4 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_PUBLIC_KEY, SUPABASE_URL } from "@/lib/config.ts";
+import { assert } from "@std/assert";
+
+assert(SUPABASE_URL, "SUPABASE_URL is not set");
+assert(SUPABASE_PUBLIC_KEY, "SUPABASE_ANON_KEY is not set");
 
 export const supabase = createClient(SUPABASE_URL!, SUPABASE_PUBLIC_KEY!);
